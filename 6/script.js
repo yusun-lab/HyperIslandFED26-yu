9dd9519f53ad7f35ffad5d5758640517423ab69a
yusun-lab <yu.sun@hyperisland.se>
2025-01-13 12:50:25 +0100

    Assignment 6 is done

--- 6/script.js
+++ 6/script.js
@@ -1,23 +1,49 @@
-// TODO: Set item in localStorage
+// 1. TODO: Set item in localStorage
+localStorage.setItem('language', 'JavaScript');
 
-// TODO: Get item from localStorage
+// 2.TODO: Get item from localStorage
+const language = localStorage.getItem('language');
+console.log("Getting item from localStorage: ", language);
 
-// TODO: Remove item from localStorage
+// 3. TODO: Remove item from localStorage
+localStorage.removeItem('language');
+console.log("Item 'language' removed from localStorage"); 
 
-// TODO: Clear all items from localStorage
+// 4. TODO: Clear all items from localStorage
+localStorage.clear();
+console.log("All items cleared form localStorage");
 
-// TODO: Add an object to localStorage
+// 5. TODO: Add an object to localStorage
+const user = { name: "John Doe", age: 25, skill: "React" };
+localStorage.setItem('user', JSON.stringify(user));
+console.log("Object 'user' added to localStorage: ", user);
 
-// TODO: Get an object from localStorage
+// 6. TODO: Get an object from localStorage
+const storeUser = JSON.parse(localStorage.getItem('user'));
+console.log("Getting object from localStorage: ", storeUser);
 
-// TODO: Set item in sessionStorage
+// ________________________________________________________________________
+// 7. TODO: Set item in sessionStorage
+sessionStorage.setItem('sessionLanguage', 'TypeScript');
 
-// TODO: Get item from sessionStorage
+// 8. TODO: Get item from sessionStorage
+const sessionLanguage = sessionStorage.getItem('sessionLanguage');
+console.log("Getting item from sessionStorage: ", sessionLanguage);
 
-// TODO: Remove item from sessionStorage
+// 9. TODO: Remove item from sessionStorage
+sessionStorage.removeItem('sessionLanguage');
+console.log("Item 'sessionLanguage' removed from sessionStorage");
 
-// TODO: Clear all items from sessionStorage
+// 10. TODO: Clear all items from sessionStorage
+sessionStorage.clear();
+console.log("All items cleared from sessionStorage");
 
-// TODO: Add an object to sessionStorage
+// 11. TODO: Add an object to sessionStorage
+const sessionUser = { name: "Jane Smith", age: 30, skill: "Vue" };
+sessionStorage.setItem('sessionUser', JSON.stringify(sessionUser));
+console.log("Object 'sessionUser' added to sessionStorage: ", sessionUser);
 
-// TODO: Get an object from sessionStorage
+// 12. TODO: Get an object from sessionStorage
+const storeSessionUser = JSON.parse(sessionUser.getItem('sessionUser'));
+console.log("Getting object from sessionStorage: ", storeSessionUser);
+
